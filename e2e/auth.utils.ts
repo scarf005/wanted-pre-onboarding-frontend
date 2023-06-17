d6526baf1344513f066012b2e1a2dbdbb5044228
scarf <greenscarf005@gmail.com>
2023-06-17 18:16:03 +0900

    test: 이메일 및 비밀번호 테스트 강화
    @만 있는 경우도 확인

--- e2e/auth.utils.ts
+++ e2e/auth.utils.ts
@@ -1,8 +1,8 @@
 import { Page } from "@playwright/test"
 import { genRandomValidEmail, genRandomValidPassword } from "./gen.utils"
 
-const getEmailInput = (page: Page) => page.getByTestId("email-input")
-const getPasswordInput = (page: Page) => page.getByTestId("password-input")
+export const getEmailInput = (page: Page) => page.getByTestId("email-input")
+export const getPasswordInput = (page: Page) => page.getByTestId("password-input")
 export const getSignInButton = (page: Page) => page.getByTestId("signin-button")
 
 export const fillAuth = async (
--- e2e/gen.utils.ts
+++ e2e/gen.utils.ts
@@ -1,9 +1,24 @@
-export const genRandomNumstring = (min: number, max: number) => {
-  const length = Math.floor(Math.random() * (max - min + 1)) + min
-  return Array.from({ length }, () => Math.floor(Math.random() * 10)).join("")
+export const randomElement = <T>(arr: T[]) =>
+  arr[Math.floor(Math.random() * arr.length)]
+
+export const randomUnicode = () =>
+  String.fromCharCode(Math.floor(Math.random() * 65535))
+
+export const randomNum = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min
+
+export const randomString = (length: number) =>
+  Array.from({ length }, randomUnicode).join("")
+
+export const randomUnicodeWithoutAt = (length: number) =>
+  [...randomString(length)].map((x) => (x === "@" ? "뷁" : x)).join("")
+
+export const genRandomValidEmail = () => {
+  const [begin, end] = [
+    randomUnicodeWithoutAt(randomNum(1, 10)),
+    randomUnicodeWithoutAt(randomNum(1, 10)),
+  ]
+  return randomElement([`${begin}@`, `@${end}`, `${begin}@${end}`])
 }
 
-export const genRandomValidEmail = () =>
-  `${genRandomNumstring(1, 10)}@${genRandomNumstring(1, 10)}`
-
-export const genRandomValidPassword = () => genRandomNumstring(8, 20)
+export const genRandomValidPassword = () => randomString(randomNum(8, 20))
